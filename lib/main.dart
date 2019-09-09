65c632b21680aa1de06f367a0761320841574e6c
UTKARSH RAJ <rajutkarshai@gmail.com>
2019-09-09 16:19:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: true,
+    title: 'Carousel Pro',
+    home: CarouselPage(),
+  ));
+}
+
+class CarouselPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 150.0,
+          width: 300.0,
+          child: Carousel(
+            boxFit: BoxFit.cover,
+            autoplay: false,
+            animationCurve: Curves.fastOutSlowIn,
+            animationDuration: Duration(milliseconds: 100),
+            dotSize: 6.0,
+            dotIncreasedColor: Color(0xFFFF335C),
+            dotBgColor: Colors.transparent,
+            dotPosition: DotPosition.topRight,
+            dotVerticalPadding: 10.0,
+            showIndicator: true,
+            indicatorBgPadding: 7.0,
+            images: [
+              NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+              NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+              NetworkImage("assets/images/LaunchImage.jpg"),
+            ],
+            autoplayDuration: Duration(seconds: 2),
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
